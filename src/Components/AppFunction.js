bbd70e65761cce9fc6e3267a1e7944f5f26d5832
RussellShire <russell.shire@gmail.com>
2022-11-16 15:59:09 +0000

    Add hookup pieces ready for funcitonality

---
+++ src/Components/AppFunction.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import NewTask from "./NewTask";
+import TasksList from "./TasksList";
+
+function AppFunction() {
+    return (
+        <p>hello world</p>
+        )
+}
+
+export default AppFunction;
