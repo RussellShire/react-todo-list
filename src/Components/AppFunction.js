cc7131f119bb465632cb60e9f2544b5d4b560a5d
RussellShire <russell.shire@gmail.com>
2022-11-16 16:23:35 +0000

    Add handleChange and handleSubmit to AppFunction

--- src/Components/AppFunction.js
+++ src/Components/AppFunction.js
@@ -1,11 +1,50 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import NewTask from "./NewTask";
 import TasksList from "./TasksList";
 
 function AppFunction() {
+    const [newTask, setNewTask] = useState({})
+    const [allTasks, setAllTasks] = useState([])
+
+    const handleChange = ({ target }) => {
+        const { name, value } = target;
+        
+        setNewTask((prev) => ({ 
+          ...prev,
+          [name]: value,
+          id: Date.now()
+        }));
+      };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setAllTasks((prev) => {
+            return [newTask, ...prev]
+        })
+        setNewTask({ 
+            title: '',
+            id: ''
+        });
+    }
+
+    const handleDelete = () => {
+
+    }
+
     return (
-        <p>hello world</p>
-        )
+        <main>
+            <h1>Tasks</h1>
+            <NewTask
+                newTask={newTask}
+                handleChange={handleChange}
+                handleSubmit={handleSubmit}
+            />
+            <TasksList
+                allTasks={allTasks}
+                handleDelete={handleDelete}
+            />
+        </main>
+    )
 }
 
 export default AppFunction;
